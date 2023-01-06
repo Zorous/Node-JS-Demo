d61b2dacd0e6f5609c067fd40897750b203e2b7f
Zorous <oussamabelhadi5@gmail.com>
2023-01-06 22:54:20 +0100

    explication

--- Node JS Server with Pipes/PippingInto.js
+++ Node JS Server with Pipes/PippingInto.js
@@ -9,6 +9,11 @@ var server = http.createServer(function(req,res){
 
     res.writeHead(200, {'Content-Type' : 'text/plain'});
 var readStream = fs.ReadStream(__dirname+'/readme.txt','utf-8');
+
+//here instead of pipping it into a local file, we will pipe i into the server's response
+
+//The benefit of this is every time we get a chunk of data we send it to the user using the pipe method so the app will be tooooo fast
+
     readStream.pipe(res);
 
 
